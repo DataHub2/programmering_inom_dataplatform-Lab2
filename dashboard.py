46526e93e60e42a3c6c7b353bb009154ffcc0e2e
Hannah Zemack <hannah.zemack@gmail.com>
2026-03-23 09:51:52 +0100

    add supabase connection in dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,15 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client
 import streamlit as st
+
+load_dotenv()  # laddar in .env filen
+
+@st.cache_resource
+def init_supabase():
+    url = os.getenv("SUPABASE_URL")
+    key = os.getenv("SUPABASE_KEY")
+    return create_client(url, key)
 
 # title
 st.title("riksdags_data")
